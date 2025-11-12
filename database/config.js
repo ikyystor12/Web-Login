5a5f71b4927e95f1a577005a0968faf71785f8ec
ikyystor12 <ikyyajaa10@gmail.com>
2025-11-12 09:30:09 +0700

    Add files via upload

---
+++ database/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8291441292:AAEUWfuAiZuUwmvx5BKoEeA9NTKlahTcxOA",  // Ubah Jadi Token Bot Mu !!!
+  owner: "8171004463", // Ubah Jadi Id Mu !!!
+  port: "2050", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "152.42.180.70" // Ubah Jadi Ip Vps Mu !!!
+};
